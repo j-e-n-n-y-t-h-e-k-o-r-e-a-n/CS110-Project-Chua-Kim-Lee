13e397f6a897baaf548379b5c6927414978c1c17
Kimberly Anne Chua <45088930+potetz@users.noreply.github.com>
2018-11-27 21:45:11 +0800

    help pls huhu idk anymore

--- NodeManager.java
+++ NodeManager.java
@@ -21,11 +21,31 @@ public class NodeManager {
     //all new nodes have at least 2 keys
     //num 1 = less
     //num2 = greater
+    public void changeNode(long location,RandomAccessFile db, long parent, long num1, long num2, long off1, long off2)throws IOException{
+        db.seek(location);
+        db.writeLong(parent);
+        for(int i=0;i<11;i++){
+            switch (i) {
+                case 1: //write 1st child and offset in proper place
+                    db.writeLong(num1); //write the first num in slot 1
+                    db.writeLong(off1); //write its parent
+                    break;
+                case 4: //write 2nd child and offset in proper place
+                    db.writeLong(num2); //write 2nd num in slot 2
+                    db.writeLong(off2);
+                    break;
+                default:
+                    db.writeLong(-1);
+                    break;
+            }
+        }
+        
+    }
     public void addNode(RandomAccessFile db,long parent,long numNodes,long num1,long num2,long off1,long off2)throws IOException{
         long lastRecord = 112*numNodes+16;
         db.seek(lastRecord); //go to the end of the latest record (EOF)
         db.writeLong(parent); //write the parent
-        for(int i=0;i<13;i++){
+        for(int i=0;i<11;i++){
             switch (i) {
                 case 1: //write 1st child and offset in proper place
                     db.writeLong(num1); //write the first num in slot 1
@@ -71,11 +91,14 @@ public class NodeManager {
         //before even sorting arr, we make a clone of it THAT IS STILL PAIRED WIF ITS OFFSETS
         //so we can use that (^^^^) pairing to be able to fix the keys' order
         //WITH REGARDS to the sorted array
+        //NOTE TO SELF: make 1 new array (top) (handleparent) and another (right)
+        //just use current for left
         long mid = arr[2];
         long midoff = offsets2[2];
         db.seek(location);
-        addNode(db,mid,numNodes,arr[0],arr[1],offsets2[0],offsets2[1]); //id = numnodes-2 (left node)
+        //addNode(db,mid,numNodes,arr[0],arr[1],offsets2[0],offsets2[1]); //id = numnodes-2 (left node)
         addNode(db,mid,numNodes,arr[3],arr[4],offsets2[3],offsets2[4]); //id = numnodes-1 (right node)
+        db.
         handleParent(p,db,mid,midoff,numNodes);
 //        db.writeLong(numNodes-2);
 //        db.writeLong(mid);
